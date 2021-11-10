14fd3b7738543ccd4f847ec8e729c9ac8a1bcc5d
Miguel Bacchin <bacchinm@gmail.com>
2021-11-10 13:17:38 -0300

    .

--- Checkpoint-I/checkpoint.js
+++ Checkpoint-I/checkpoint.js
@@ -1,21 +1,33 @@
-let pipoca = 10
-let macarrao = 8
-let carne = 15
-let feijao = 12
-let brigadeiro = 8
+function microondas(prato, tempoEscolhido); {
+let comida = prato
+let tempo = 0
 
-function padrao(opcao);
-    switch(opcao){
-            case "Pipoca":
-            return 10;
-            case "Macarrão":
-            return 8;
-            case "Carne":
-            return 15;
-            case "Feijão": 
-            return 12;
-            case "Brigadeiro":
-            return 8;
+    switch(comida){
+            case "pipoca":
+                tempo = 10;
+            break
+            case "macarrao":
+                tempo = 8;
+            break
+            case "carne":
+                tempo = 15;
+            break
+            case "feijao": 
+                tempo = 12;
+            break
+            case "brigadeiro":
+                tempo = 8;
+            break
             default:
-            return "Prato inexistente";
+            return "prato inexistente";
     }
+    if (tempoEscolhido >= 0 && tempoEscolhido < tempo){
+            return tempoEscolhido = "tempo insuficiente"
+    } else if (tempoEscolhido >= (tempo*2) && tempoEscolhido < (tempo*3)){
+            return "a comida queimou"
+    } else if (tempoEscolhido >= tempo &&  tempoEscolhido < (tempo*2)){
+            return "prato pronto, bom apetite!!!"
+    }else if (tempoEscolhido >= (tmepo*3)){
+            return "kabumm"
+    }
+}

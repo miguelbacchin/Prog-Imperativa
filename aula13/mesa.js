7b4cd3eeee731e9958796e7f98ea1582b63910b2
miguelbacchin <bacchinm@gmail.com>
2021-11-24 22:19:57 -0300

    Update mesa.js

--- aula13/mesa.js
+++ aula13/mesa.js
@@ -13,6 +13,20 @@ function Usuario (numeroContaParam,tipoContaParam,saldoParam,titularParam){
     this.titular = titularParam
 }
 
+this.formataSaldo = function(){
+    return "R$ " + this.saldo.toLocaleString('pt-BR');
+}
+
+let conta1 = {
+    numero:3151956165,
+    tipo:'Conta Corrente',
+    saldo:7601,
+    titular:'Alonso Wannan',
+    formataSaldo:function(){
+        return "R$ " + this.saldo.toLocaleString('pt-BR');
+    }
+}
+
 const abigael = new Usuario (5486273622,'Conta Corrente',27771,'Abigael Natte');
 const ramon = new Usuario (1183971869,'Conta Poupança',8675,'Ramon Connell');
 const jarret = new Usuario (9582019689,'Conta Poupança',27363,'Jarret Lafuente');
@@ -32,5 +46,42 @@ const banco = {
 
     }
 }
+/* console.log(conta1.saldo);
+console.log(conta1.formataSaldo()); */
 
-console.log(array);
+const lista = [conta1,conta2,conta3];
+
+for(let i = 0; i<lista.length;i++){
+    const numeroConta = i;
+    const tipoConta = i % 2 == 0 ? 'Conta Corrente' : 'Conta Poupança';
+    const saldo = Math.random() * 10000;
+    const titular = 'Cliente' + i;
+    const conta = new Conta(numeroConta,tipoConta,titular);
+    lista.push(conta);
+}
+
+/* ou você pode fazer */
+
+for(let i in lista){
+    const conta = lista[i];
+}
+
+for(let conta of lista){
+
+}
+
+const numeros = [1,2,3];
+for(let numero of numeros){
+    console.log(numero);
+}
+
+const banco = {
+    clientes:lista,
+    consultarCliente:function(titularABuscar){
+        for(let cliente of TouchList,clientes){
+            if(cliente.titular == titularABuscar){
+                return cliente;
+            }
+        }
+    }
+}

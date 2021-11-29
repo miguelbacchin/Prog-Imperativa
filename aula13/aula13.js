d14f2e31c5c7a2a22b360c6c348faa1ac2877ab4
Miguel Bacchin <bacchinm@gmail.com>
2021-11-29 09:20:59 -0300

    .

--- aula13/aula13.js
+++ aula13/aula13.js
@@ -1,5 +1,33 @@
-// Objetos
-// JSON - JavaScript Objejact Notation (JSON)
+// Objetos // 
+
+// Exemplo 01
+let meuPais = {
+    nome: "Brasil",
+    populacao: 202768562,
+    capital: "Brasilia",
+    dizerNacionalidade: function () {
+        return "Nasci no " + this.nome;
+    }
+};
+// console.log('A capital do ' + meuPais.nome + ' é ' + meuPais.capital)
+// console.log(meuPais.dizerNacionalidade())
+
+// Exemplo 02
+let carro = {
+    marca: 'Fiat',
+    modelo: 'Uno',
+}
+
+function Carro(valorMarca, valorModelo){
+    this.marca = valorMarca;
+    this.modelo = valorModelo;
+}
+// console.log(carro);
+
+// console.log(new Carro("Renault", "Clio"));
+
+
+// JSON - JavaScript Objejact Notation (JSON) //
 const usuario = {
     nome:'Miguel',
     email:'bacchinm@gmail.com',
